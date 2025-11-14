c99a759541e3d76711fd0ad3b17b8692ae453d94
Your Name <your.email@example.com>
2025-11-14 10:54:12 +0200

    added update(patch) functionality(api part)

--- TrainingTracker_API/obj/Debug/net8.0/TrainingApp.API.AssemblyInfo.cs
+++ TrainingTracker_API/obj/Debug/net8.0/TrainingApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrainingApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fbf052eadb5beabf7f93265a0ea4552e44594a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a6d5b62d574e2efcacb887ee1cc12b09571502")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrainingApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrainingApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
